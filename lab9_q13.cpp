c2308fe6448be6795b15badc82c9531e0f281b31
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-11-09 22:23:06 +0530

    Add files via upload

---
+++ lab9_q13.cpp
@@ -0,0 +1,37 @@
+//including the library
+
+#include<iostream>
+using namespace std;
+
+//main function
+int main()
+{
+    //declaring array 
+    int ak [10]={1,2,3,4,5,6,7,8,9,10};
+
+    //pointer
+    int *y;
+
+    //1st case
+    cout<<"Print the array  by normal index method"<<endl;
+
+    // loop
+    for(int i=0;i<10;i++){
+    cout<<ak[i]<<" "<<endl;
+    }
+    
+    //2nd case
+    cout<<"Print the array by pointer method"<<endl;
+
+    //pointing 
+    y=ak;
+    
+    
+    for(int i=0;i<10;i++){
+    cout<<*y<<endl;
+    y++;
+    }
+    
+return 0;
+}
+
