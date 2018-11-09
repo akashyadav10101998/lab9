34765688c0b463786d80a91749298c354e591ef4
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-11-09 23:42:23 +0530

    Add files via upload

---
+++ lab9_q9.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+string contains(char arr[], char x )
+	{
+		char * poi = arr;
+		int counter= 0;
+		string c= "true", d= "false";
+		for(; *poi != '\0'; poi++)
+			{
+				counter ++;
+				if (*poi == x)
+				break;
+			}
+		if (counter == 9)
+		return d;
+		else return c;
+	}
+	
+int main ()
+	{
+		char x;
+		char arr[]= {"akash"};
+		cout << " letter of my name"<<endl;
+		cin >> x;
+		cout<< contains(arr , x )<< endl;
+		return 0;
+		
+	}
